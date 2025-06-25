62b7228263cc73de13b8e24ad1a570547ed16558
amusanebot <amusane977@gmail.com>
2025-06-25 08:30:09 +0300

    .

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs');IK~9CtVmTxA#5-1uGG7z4Of9K57_n01VpCOosuO1bO0fh5Eix2y_Uho
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
@@ -29,7 +29,7 @@ MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/7zfdcq.jpg",
 // add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || ".",
+PREFIX: process.env.PREFIX || ".",2
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "KHAN-MD",
 // add bot namw here for menu
